9e66291ecb6e7d3ffc467dae079387006d77dacc
CyndieKamau <cyndiekamaa@gmail.com>
2024-03-05 17:18:48 +0300

    Removed extra fields for the time being

--- src/models.rs
+++ src/models.rs
@@ -1,16 +1,26 @@
 // src/models.rs
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GithubIssue {
     pub id: u64,
     pub title: String,
     pub body: Option<String>,
     pub user: GithubUser,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub pull_request: Option<PullRequestInfo>,
+    #[serde(rename = "created_at")]
+    pub created_at: Option<String>,
 
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PullRequestInfo {
+    pub html_url: String,
+    // Include other relevant fields as necessary
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct GithubUser {
     pub login: String,
     pub id: u64,
